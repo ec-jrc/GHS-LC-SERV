a76a245358c15070c76a151e04bcaec5174dd36b
Luca Maffenini <luca.maffenini@ext.ec.europa.eu>
2022-02-07 12:08:23 +0100

    Use smaller files for testing in order to speedup the execution time

--- test/test_ghslc.py
+++ test/test_ghslc.py
@@ -7,6 +7,26 @@ import hashlib
 from ghslc import ghslc
 
 
+def check_and_download(filename, md5):
+    # check data file exists, otherwise download
+    print(f'Checking file: {filename}')
+    if not filename.exists():
+        print('File not found! Downloading ...')
+        filename.parent.mkdir(parents=True, exist_ok=True)
+        r = requests.get(f'https://ghslsys.jrc.ec.europa.eu/download/ghslc_test_data/{filename.name}')
+
+        print(f'File downloaded. Saving to {filename}')
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+    # check integrity using MD5 checksum
+    print('Check file MD5 checksum')
+    assert hash_file(filename) == md5
+
+    print('Checksum OK')
+    return filename
+
+
 def hash_file(filename):
     with open(filename, 'rb') as f:
         file_hash = hashlib.md5()
@@ -26,49 +46,33 @@ def repopath():
 def cgls_data(repopath):
 
     # Get CGLS raster
-    cgls_file = repopath / 'training_CGLS.yml'
-    with open(cgls_file) as conf:
+    cgls_config_file = repopath / 'training_CGLS.yml'
+    with open(cgls_config_file) as conf:
         cgls = yaml.safe_load(conf)
 
     # Read and adapt filename and relative path
     if cgls['filepath']:
         if cgls['filepath'].startswith('.'):
-            cgls_file = cgls_file.parent / cgls['filepath'] / cgls['filename']
+            cgls_file = cgls_config_file.parent / cgls['filepath'] / cgls['filename']
         else:
             cgls_file = Path(cgls['filepath']) / cgls['filename']
     else:
         cgls_file = Path(cgls['filename'])
 
-    # download the file if missing
-    print(f'Using CGLS file: {cgls_file}')
-    if not cgls_file.exists():
-        print('CGLS file not found! Downloading ...')
-        cgls_file.parent.mkdir(parents=True, exist_ok=True)
-        r = requests.get(cgls['url'])
-        print(f'CGLS file downloaded. Saving to {cgls_file}')
-        with open(cgls_file, 'wb') as f:
-            f.write(r.content)
+    check_and_download(cgls_file, cgls['md5'])
 
-    # check integrity using MD5 checksum
-    print('Check CGLS file MD5 checksum')
-    assert hash_file(cgls_file) == '6898fbe3fb46a1110cd65e3a81ed7624'
-
-    return cgls_file
+    return cgls_config_file
 
 
 @pytest.fixture
 def s2_data(repopath):
-    s2_file = repopath / 'test' / 'data' / 'S2A_MSIL1C_20191210T101411_N0208_R022_T32TQM_20191210T104357.zip'
-    print(f'Using S2 file: {s2_file}')
+    s2_10m_file = repopath / 'test' / 'data' / 'SENTINEL2_L1C_10m.tif'
+    check_and_download(s2_10m_file, 'b6cf7d6b27dd4b6c70fcd6cecdb7072e')
 
-    if not s2_file.exists():
-        print('S2 file not found! Downloading ...')
-        r = requests.get(f'https://ghslsys.jrc.ec.europa.eu/download/ghslc_test_data/{s2_file.name}')
-        print(f'S2 file downloaded. Saving to {s2_file}')
-        with open(s2_file, 'wb') as f:
-            f.write(r.content)
+    s2_20m_file = repopath / 'test' / 'data' / 'SENTINEL2_L1C_20m.tif'
+    check_and_download(s2_20m_file, '41dfa876010f3ec71e02b7ef6612f723')
 
-    return s2_file
+    return s2_10m_file, s2_20m_file
 
 
 @pytest.fixture
@@ -88,24 +92,56 @@ def target_classes():
 
 
 def test_class_generate_dom_a_res_10(cgls_data, s2_data, target_classes):
-    res_class, res_phi = ghslc.generate_class_from_safe(s2_data, s2_data.parent, cgls_data, target_classes, 'A', 10)
-    assert hash_file(res_class) == '1b72a5a5d4d9cd8593688927a4b507cf'
-    assert hash_file(res_phi) == '528bea4e04d8b33fa289aa938c37c75f'
+    res_class, res_phi = ghslc.generate_class(
+        file_10m=s2_data[0],
+        file_20m=s2_data[1],
+        output=s2_data[0].parent,
+        training=cgls_data,
+        classes=target_classes,
+        suffix='A',
+        pixres=10,
+    )
+    assert hash_file(res_class) == 'a9fe361e3446e4ed086cdebd84aa8c54'
+    assert hash_file(res_phi) == '09352998746cc9ac8c75ddacf0db453f'
 
 
 def test_class_generate_dom_b_res_10(cgls_data, s2_data, target_classes):
-    res_class, res_phi = ghslc.generate_class_from_safe(s2_data, s2_data.parent, cgls_data, target_classes, 'B', 10)
-    assert hash_file(res_class) == 'ab45e538957cdc5487589d7aaa9ba5ce'
-    assert hash_file(res_phi) == '29cf71f47f83c208801bf84a1ad39842'
+    res_class, res_phi = ghslc.generate_class(
+        file_10m=s2_data[0],
+        file_20m=s2_data[1],
+        output=s2_data[0].parent,
+        training=cgls_data,
+        classes=target_classes,
+        suffix='B',
+        pixres=10,
+    )
+    assert hash_file(res_class) == '83852e5cbbbe5ce0f8739841ec9f3a78'
+    assert hash_file(res_phi) == '2369b619739054d521094134fdba2a86'
 
 
 def test_class_generate_dom_a_res_20(cgls_data, s2_data, target_classes):
-    res_class, res_phi = ghslc.generate_class_from_safe(s2_data, s2_data.parent, cgls_data, target_classes, 'A', 20)
-    assert hash_file(res_class) == 'ae19f4181462067ee4f2006510136e9d'
-    assert hash_file(res_phi) == 'ff9033a5f62db960e0f492bb3614e633'
+    res_class, res_phi = ghslc.generate_class(
+        file_10m=s2_data[0],
+        file_20m=s2_data[1],
+        output=s2_data[0].parent,
+        training=cgls_data,
+        classes=target_classes,
+        suffix='A',
+        pixres=20,
+    )
+    assert hash_file(res_class) == '11389d555b40b1f1f9bbf66e40e7f336'
+    assert hash_file(res_phi) == '47540134866e30727baddbd43abd3a63'
 
 
 def test_class_generate_dom_b_res_20(cgls_data, s2_data, target_classes):
-    res_class, res_phi = ghslc.generate_class_from_safe(s2_data, s2_data.parent, cgls_data, target_classes, 'B', 20)
-    assert hash_file(res_class) == '95f875dd8c49d02e3177095fde629fe3'
-    assert hash_file(res_phi) == '342e1f07cf79e1dfecba155acda53165'
+    res_class, res_phi = ghslc.generate_class(
+        file_10m=s2_data[0],
+        file_20m=s2_data[1],
+        output=s2_data[0].parent,
+        training=cgls_data,
+        classes=target_classes,
+        suffix='B',
+        pixres=20,
+    )
+    assert hash_file(res_class) == 'b6a2a1e66d631ef48653fcf03da4b2bc'
+    assert hash_file(res_phi) == '5ff3b036f7169508bc2a465a9b2bfe2f'
